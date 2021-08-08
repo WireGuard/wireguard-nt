3b01a5096b43c30d6c75fb92d9e2686f53c377be
Jason A. Donenfeld <Jason@zx2c4.com>
2021-08-08 16:41:43 +0200

    api: rundll32: make empty string if no instance id
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- api/rundll32.c
+++ api/rundll32.c
@@ -87,7 +87,7 @@ VOID __stdcall CreateAdapter(HWND hwnd, HINSTANCE hinst, LPSTR lpszCmdLine, int 
         WireGuardCreateAdapter(Argv[2], Argv[3], Argc > 4 ? &RequestedGUID : NULL, &RebootRequired);
     DWORD LastError = Adapter ? ERROR_SUCCESS : GetLastError();
     WriteFormatted(
-        STD_OUTPUT_HANDLE, L"%1!X! %2!s! %3!X!", LastError, Adapter ? Adapter->DevInstanceID : L"", RebootRequired);
+        STD_OUTPUT_HANDLE, L"%1!X! %2!s! %3!X!", LastError, Adapter ? Adapter->DevInstanceID : L"\"\"", RebootRequired);
     if (Adapter)
         WireGuardFreeAdapter(Adapter);
 
